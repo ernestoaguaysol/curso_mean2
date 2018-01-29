0bef12d6df2214e6db428e6c62edc83c75f7bdfc
Ernesto <ernestoaguaysol@gmail.com>
2018-01-29 16:53:17 -0300

    update user

--- controllers/user.js
+++ controllers/user.js
@@ -82,8 +82,27 @@ function loginUser(req, res) {
     })
 }
 
+function updateUser(req, res) {
+    var userId = req.params.id;
+    var update = req.body;
+
+    User.findByIdAndUpdate(userId, update, (err, userUpdated) => {
+        if (err) {
+            res.status(500).send({ message: 'error al actualizar el usuario'});
+        }else {
+            if (!userUpdated) {
+                res.status(404).send({ message: 'no se ha podido actualizar el usuario'});                
+            } else {
+                res.status(200).send({ user: userUpdated });            
+            }
+        }
+
+    });
+}
+
 module.exports = {
     pruebas,
     saveUser,
-    loginUser
+    loginUser,
+    updateUser
 };
--- routes/user.js
+++ routes/user.js
@@ -9,5 +9,6 @@ var md_auth = require('../middlewares/authenticated')
 api.get('/probando-controlador', md_auth.enasureAuth , UserController.pruebas);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
+api.put('/update-user/:id', md_auth.enasureAuth, UserController.updateUser);
 
 module.exports = api;
