a2f1dbb2a1dbb4259a26d12cf61412e94be26857
Ernesto <ernestoaguaysol@gmail.com>
2018-01-30 14:50:03 -0300

    get save gets from Album

---
+++ routes/album.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var express = require('express');
+var AlbumController = require('../controllers/album');
+var api = express.Router();
+var md_auth = require('../middlewares/authenticated');
+
+var multipart = require('connect-multiparty');
+var md_upload = multipart({ uploadDir: './uploads/albums' });
+
+api.get('/album/:id', md_auth.enasureAuth , AlbumController.getAlbum);
+api.post('/album', md_auth.enasureAuth , AlbumController.saveAlbum);
+api.get('/albums/:artist?', md_auth.enasureAuth , AlbumController.getAlbums);
+// api.put('/album/:id', md_auth.enasureAuth , AlbumController.updateAlbum);
+// api.delete('/album/:id', md_auth.enasureAuth , AlbumController.deleteAlbum);
+// api.post('/upload-image-album/:id', [md_auth.enasureAuth, md_upload], AlbumController.uploadImage);
+// api.get('/get-image-album/:imageFile', AlbumController.getImageFile);
+
+module.exports = api;
