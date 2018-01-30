d2c702a379ae1b5b24901a626ef40546ce123237
Ernesto <ernestoaguaysol@gmail.com>
2018-01-30 12:42:26 -0300

    imagen artist post y get

--- controllers/artist.js
+++ controllers/artist.js
@@ -102,7 +102,7 @@ function deleteArtist(req, res) {
             if (!artistRemoved) {
                 res.status(404).send({ message: 'el artista no ha sido eliminado'});            
             } else {
-                
+
                 Album.find({artist: artistRemoved._id}).remove((err, albumRemoved) => {
                     if (err) {
                         res.status(500).send({ message: 'Error al eliminar album'});            
@@ -130,10 +130,58 @@ function deleteArtist(req, res) {
     })
 }
 
+function uploadImage(req, res) {
+    var artistId = req.params.id;
+    var file_name = 'No subido..';
+
+    if (req.files) {
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1].toLocaleLowerCase();
+
+        if (file_ext == 'jpg' || file_ext == 'png' || file_ext == 'gif') {
+            Artist.findByIdAndUpdate(artistId, { image: file_name }, (err, artistUpdated) => {
+                if (err) {
+                    res.status(500).send({ message: 'error al actualizar imagen'});
+                }else {
+                    if (!artistUpdated) {
+                        res.status(404).send({ message: 'no se ha podido actualizar el usuario'});                
+                    } else {
+                        res.status(200).send({ artist: artistUpdated });            
+                    }
+                }
+            });
+        } else {
+            res.status(200).send({ message: 'extencion de imagen no válida'})
+        }
+
+        console.log(file_split);
+    } else {
+        res.status(200).send({ message: 'No ha subido ningula imagen...'})
+    }
+}
+
+function getImageFile(req, res) {
+    var imageFile = req.params.imageFile;
+    var path_file = './uploads/artists/'+imageFile;
+    fs.exists(path_file, function(exists) {
+        if (exists) {
+            res.sendFile(path.resolve(path_file));
+        } else {
+            res.status(200).send({ menssage: 'No existe la imagen...'});
+        }
+    });
+}
+
 module.exports = {
     getArtist,
     saveArtist,
     getArtists,
     updateArtist,
-    deleteArtist
+    deleteArtist,
+    uploadImage,
+    getImageFile
 }
--- routes/artist.js
+++ routes/artist.js
@@ -5,10 +5,15 @@ var ArtistController = require('../controllers/artist');
 var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
+var multipart = require('connect-multiparty');
+var md_upload = multipart({ uploadDir: './uploads/artists' });
+
 api.get('/artist/:id', md_auth.enasureAuth , ArtistController.getArtist);
 api.post('/artist', md_auth.enasureAuth , ArtistController.saveArtist);
 api.get('/artists/:page?', md_auth.enasureAuth , ArtistController.getArtists);
 api.put('/artist/:id', md_auth.enasureAuth , ArtistController.updateArtist);
 api.delete('/artist/:id', md_auth.enasureAuth , ArtistController.deleteArtist);
+api.post('/upload-image-artist/:id', [md_auth.enasureAuth, md_upload], ArtistController.uploadImage);
+api.get('/get-image-artist/:imageFile', ArtistController.getImageFile);
 
 module.exports = api;
