bd6438871c0c2eb5f06a6069439a7ef0f3680100
Ernesto <ernestoaguaysol@gmail.com>
2018-02-01 01:12:16 -0300

    bug header app.js

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(bodyParser.json());
 // configurar cabeceras http
 
 app.use((req, res, next) => {
-    req.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Origin','*');
+
     res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow','GET, POST, OPTIONS, PUT, DELETE');
